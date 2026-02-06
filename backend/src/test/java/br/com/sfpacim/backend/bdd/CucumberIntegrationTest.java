28044aaa83722ce097f43a767ab93a6e49b94789
Matheus Pereira <matheusfnpereira@gmail.com>
2026-02-06 19:15:56 -0300

    chore(backend): migra execução para JUnit 5 e configura suporte no VS Code
    - Migra o motor de execução de testes BDD de JUnit 4 para JUnit 5 (Platform Engine), atualizando as dependências no 'pom.xml'.
    - Refatora a classe 'CucumberIntegrationTest' para utilizar a anotação '@Suite' e '@SelectClasspathResource', substituindo o antigo '@RunWith'.
    - Ajusta 'settings.json' para mapear steps e features Java, habilitando o autocomplete e navegação (Ctrl+Click) da extensão 'Cucumber (Gherkin) Full Support'.
    - Adiciona o relatório HTML do Cucumber ('cucumber-reports.html') aos artefatos de upload da Pipeline de CI/CD.

--- backend/src/test/java/br/com/sfpacim/backend/bdd/CucumberIntegrationTest.java
+++ backend/src/test/java/br/com/sfpacim/backend/bdd/CucumberIntegrationTest.java
@@ -1,18 +1,21 @@
 package br.com.sfpacim.backend.bdd;
 
-import org.junit.runner.RunWith;
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.platform.engine.Constants;
 
 /**
- * Classe "Runner" (Executora) que permite ao JUnit 4 (Vintage) descobrir
+ * Classe "Runner" (Executora) que permite ao JUnit 5 descobrir
  * e executar os testes do Cucumber (arquivos .feature).
- *
- * @author Matheus F. N. Pereira
  */
-@RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/features", glue = "br.com.sfpacim.backend.bdd")
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
+@ConfigurationParameter(key = Constants.GLUE_PROPERTY_NAME, value = "br.com.sfpacim.backend.bdd")
+@ConfigurationParameter(key = Constants.PLUGIN_PROPERTY_NAME, value = "pretty, html:target/cucumber-reports.html")
 public class CucumberIntegrationTest {
 
 }
